f611b92b1159338d968b6f2fecb2492b62cbbfdd
Scott Stephens <stephens.js@gmail.com>
2018-10-13 22:19:12 -0500

    --allow-empty-message

--- deploy.ps1
+++
@@ -1,5 +0,0 @@
-remove-item -path ..\scottstephens.github.io\* -exclude .git -recurse
-copy-item _site\* ..\scottstephens.github.io -recurse
-git -C ..\scottstephens.github.io add --all
-git -C ..\scottstephens.github.io commit -m "" --allow-empty-message
-git -C ..\scottstephens.github.io push
